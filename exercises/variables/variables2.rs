474904b58791d42b87bd4ce1aca136b64fae0b4d
rocosodev <rocosodev@gmail.com>
2022-11-08 19:37:28 -0600

    needed to change variables2

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -2,7 +2,7 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    let x;
+    let x = 5;
     if x == 10 {
         println!("x is ten!");
     } else {
